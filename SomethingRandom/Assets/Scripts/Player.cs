2bf44aab35ae21ecbf3b3a6a88d6dbcafaa9ef33
ImGettingForcedToDoThis <47280941+ImGettingForcedToDoThis@users.noreply.github.com>
2019-02-23 11:23:03 -0800

    hi
    ur not cool

---
+++ SomethingRandom/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float Speed
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (input.GetKeyDown(KeyCode.W))
+
+		{
+
+			transform.Translate (transform.position.z + Speed * Time.deltaTime);
+
+
+		}
+}
