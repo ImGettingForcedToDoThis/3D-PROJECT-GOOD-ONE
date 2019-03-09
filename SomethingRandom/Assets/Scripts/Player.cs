e927d1635a5c426b90448859c0ff1568cbb13cbd
ImGettingForcedToDoThis <47280941+ImGettingForcedToDoThis@users.noreply.github.com>
2019-03-09 11:23:00 -0800

    was up
    rgvr4g

--- SomethingRandom/Assets/Scripts/Player.cs
+++ SomethingRandom/Assets/Scripts/Player.cs
@@ -3,20 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public float Speed
+	public Camera myCamera;
+	public float Speed; 
+	public float sensitivity;
+	private Vector2 Mymouse;
 	// Use this for initialization
 	void Start () {
-		
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (input.GetKeyDown(KeyCode.W))
+		Mymouse.x = Input.GetAxis("Mouse X");
+		Mymouse.y = Input.GetAxis("Mouse Y");
 
+		transform.eulerAngles += new Vector3(0.0f, Mymouse.x*sensitivity*Time.deltaTime, 0.0f);
+		if (Input.GetKey(KeyCode.W))
 		{
 
-			transform.Translate (transform.position.z + Speed * Time.deltaTime);
+			transform.Translate (0.0f,0.0f, Speed * Time.deltaTime);
 
 
 		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+
+			transform.Translate (0.0f,0.0f, - Speed * Time.deltaTime);
+
+
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+
+			transform.Translate (0.0f,0.0f, - Speed * Time.deltaTime);
+
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+
+			transform.Translate (0.0f,0.0f,  Speed * Time.deltaTime);
+
+
+		}
+
+	}
 }
