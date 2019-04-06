df7c536f9234c2fb218be62fde15984999498e33
ImGettingForcedToDoThis <47280941+ImGettingForcedToDoThis@users.noreply.github.com>
2019-04-06 11:28:30 -0700

    13r

--- PortalGame/Assets/Scripts/Player.cs
+++ PortalGame/Assets/Scripts/Player.cs
@@ -2,52 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-	public Camera myCamera;
-	public float Speed; 
-	public float sensitivity;
-	private Vector2 Mymouse;
+public class Player : MonoBehaviour
+{
+    public Camera myCamera;
+    public float Speed;
+    public float JumpStrength;
+    public float Sensitivity;
+
+    private Vector2 myMouse;
+    private Rigidbody myRigidbody;
+    private bool isGrounded;
+
 	// Use this for initialization
-	void Start () {
-		Cursor.lockState = CursorLockMode.Locked;
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		Mymouse.x = Input.GetAxis("Mouse X");
-		Mymouse.y = Input.GetAxis("Mouse Y");
+	void Update ()
+    {
+        myMouse.x = Input.GetAxis("Mouse X");
+        myMouse.y = Input.GetAxis("Mouse Y");
 
-		transform.eulerAngles += new Vector3(0.0f, Mymouse.x*sensitivity*Time.deltaTime, 0.0f);
-		if (Input.GetKey(KeyCode.W))
-		{
+        transform.eulerAngles += new Vector3(0.0f, myMouse.x * Sensitivity * Time.deltaTime, 0.0f);
+        Vector3 targetRotation = new Vector3(-myMouse.y * Sensitivity * Time.deltaTime, 0.0f, 0.0f);
+        if(targetRotation.x + myCamera.transform.eulerAngles.x < 90 || targetRotation.x + myCamera.transform.eulerAngles.x > 270)
+        {
+            myCamera.transform.eulerAngles += targetRotation;
+        }
 
-			transform.Translate (0.0f,0.0f, Speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, 0.0f, Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, 0.0f, -Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            myRigidbody.AddForce(0.0f, JumpStrength, 0.0f, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
 
-
-		}
-
-		if (Input.GetKey(KeyCode.A))
-		{
-
-			transform.Translate (0.0f,0.0f, - Speed * Time.deltaTime);
-
-
-		}
-
-		if (Input.GetKey(KeyCode.S))
-		{
-
-			transform.Translate (0.0f,0.0f, - Speed * Time.deltaTime);
-
-		}
-
-		if (Input.GetKey(KeyCode.D))
-		{
-
-			transform.Translate (0.0f,0.0f,  Speed * Time.deltaTime);
-
-
-		}
-
-	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
 }
