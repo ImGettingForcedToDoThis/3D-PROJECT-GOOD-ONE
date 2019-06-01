54d4070436d10c7a75395777b8d916bf29209706
lazyunicorn123 <48162209+lazyunicorn123@users.noreply.github.com>
2019-06-01 11:26:51 -0700

    alloe

--- PortalGame/Assets/Scripts/Player.cs
+++ PortalGame/Assets/Scripts/Player.cs
@@ -8,30 +8,33 @@ public class Player : MonoBehaviour
     public float Speed;
     public float JumpStrength;
     public float Sensitivity;
-    public GameObject Blueportal;
-    public GameObject Orangeportal;
+
+    public GameObject BluePortal;
+    public GameObject OrangePortal;
+
     private Vector2 myMouse;
     private Rigidbody myRigidbody;
     private bool isGrounded;
+
     private GameObject ActiveBluePortal;
     private GameObject ActiveOrangePortal;
-  
+
     // Use this for initialization
-    void Start()
+    void Start ()
     {
         Cursor.lockState = CursorLockMode.Locked;
         myRigidbody = gameObject.GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
         myMouse.x = Input.GetAxis("Mouse X");
         myMouse.y = Input.GetAxis("Mouse Y");
 
         transform.eulerAngles += new Vector3(0.0f, myMouse.x * Sensitivity * Time.deltaTime, 0.0f);
         Vector3 targetRotation = new Vector3(-myMouse.y * Sensitivity * Time.deltaTime, 0.0f, 0.0f);
-        if (targetRotation.x + myCamera.transform.eulerAngles.x < 90 || targetRotation.x + myCamera.transform.eulerAngles.x > 270)
+        if(targetRotation.x + myCamera.transform.eulerAngles.x < 90 || targetRotation.x + myCamera.transform.eulerAngles.x > 270)
         {
             myCamera.transform.eulerAngles += targetRotation;
         }
@@ -57,88 +60,77 @@ public class Player : MonoBehaviour
             myRigidbody.AddForce(0.0f, JumpStrength, 0.0f, ForceMode.Impulse);
             isGrounded = false;
         }
-
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))
         {
             ShootBluePortal();
         }
-        if (Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(1))
         {
             ShootOrangePortal();
         }
-    }
-   
 
-    void ShootBluePortal
-        ()
+    }
+
+    void ShootBluePortal()
     {
         Ray ray = myCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
-
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if(Physics.Raycast(ray,out hit))
         {
+            if(ActiveBluePortal)
+            {
+                Destroy(ActiveBluePortal);
+            }
+            ActiveBluePortal = Instantiate(BluePortal, hit.point, Quaternion.LookRotation(hit.normal, Vector3.up));
         }
         
-        if (ActiveBluePortal)
-        {
-
-        }
-        ActiveBluePortal = instantiate(Blue) Blueportal; Quaternion.LookRotation(hit.normal, Vector3.up);
-
     }
 
-
-
-    Destroy(oldBlueportal); 
-
-
-    void Shootorangeportal()
-  
+    void ShootOrangePortal()
     {
         Ray ray = myCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
-
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
+            if (ActiveOrangePortal)
+            {
+                Destroy(ActiveOrangePortal);
+            }
+            ActiveOrangePortal = Instantiate(OrangePortal, hit.point, Quaternion.LookRotation(hit.normal, Vector3.up));
         }
-
-        if (ActiveOrangePortal)
-        {
-
-        }
-        Activeorangeportal = instantiate(orange) orangePortal Quaternion.LookRotation(hit.normal, Vector3.up);
-
     }
-}
+
+    void Teleport(GameObject portal)
+    {
+        GameObject otherPortal;
+        if (portal.name == "BluePortal(Clone)")
+        {
+            otherPortal = GameObject.Find("OrangePortal(Clone)");
+        }
+        else
+        {
+            otherPortal = GameObject.Find("BluePortal(Clone)");
+        }
+        if (otherPortal)
+        {
+            transform.position = otherPortal.transform.position;
 
 
-void teleport(Gameobject portal)
 
-{
-	GameObject otherportal;
+            float magnitude = myRigidbody.velocity.magnitude;
+            myRigidbody.velocity = otherPortal.transform.forward * magnitude;
+        }
+    }
 
-	if (portal.name == "BluePortal")
-	{
-		otherPortal = gameObject.Find("OrangePortal")
-	}
-	else
-	{
-		otherportal = gameObject.Find("BluePortal")
-	}
-	transform.Position = Portal.transform.position
-}
-float magnitude = myRigidbody.Velocity.magnitude;
-myRigidbody.Velocity = otherportal.transform.teleport*magnitude;
-transform.position = potal transform.position
     void OnCollisionEnter(Collision collision)
     {
-	
         if(collision.gameObject.tag == "Ground")
         {
             isGrounded = true;
         }
-	else if(Collision.gameObject.tag == "portal"){
-		teleport Collision.gamemode;
-	}
+        else if(collision.gameObject.tag == "Portal")
+        {
+            Teleport(collision.gameObject);
+        }
     }
 }
